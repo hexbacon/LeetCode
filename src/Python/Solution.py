8040a1905f09cdd64ebe97e8be68865adcd398ce
Christopher Mena <you@chrismena.dev>
2024-02-23 21:24:02 -0500

    solved twoSumm for python

--- src/Python/Solution.py
+++ src/Python/Solution.py
@@ -1,10 +1,13 @@
 def twoSum(nums, target):
+    # Create dict to hold answer
     res = dict()
     for idx, num in enumerate(nums):
-        if res.get(target - num):
-            return [idx, res.get(target - num)]
+        # if target - number is a key, return answer
+        if res.get(target - num) != None:
+            print("Found it")
+            return sorted([idx, res.get(target - num)])
+        # if answer is not found, add idx to the dict
         res[num] = idx
-        print(res)
     return [-1,-1]
 
-print(twoSum([1,2,4,5,6], 3))
+print(twoSum([1,2,4,5,6], 11))
